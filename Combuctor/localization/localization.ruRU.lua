74bdacaa1a55f634e7df97be78c1b8b0262629d8
Chkbrk <ibot2670@gmail.com>
2026-05-01 19:06:50 +0300

    Add files via upload

---
+++ Combuctor/localization/localization.ruRU.lua
@@ -0,0 +1,49 @@
+﻿
+local L = LibStub("AceLocale-3.0"):NewLocale("Combuctor", "ruRU")
+if not L then return end
+
+L.Updated = 'Обновлен'
+
+--binding actions
+L.ToggleInventory = "Инвентарь"
+L.ToggleBank = "Банк"
+
+--frame titles
+L.InventoryTitle = "Инвентарь"
+L.BankTitle = "Банк"
+
+--tooltips
+L.Inventory = 'Инвентарь'
+L.Bank = 'Банк'
+L.TotalOnRealm = 'Всего'
+L.ClickToPurchase = '<Кликните> чтобы приобрести'
+L.Bags = 'Сумки'
+L.BagToggle = ''
+L.InventoryToggle = ''
+L.BankToggle = ''
+L.MoveTip = ''
+L.ResetPositionTip = ''
+
+--default sets (need to be here because of a flaw in how I save things
+--these are automatically localized (aka, don't translate them :)
+do
+	L.All = ALL
+
+	L.Weapon, L.Armor, L.Container, L.Consumable, L.Glyph, L.TradeGood, 
+ 	L.Projectile, L.Quiver, L.Recipe, L.Gem, L.Misc, L.Quest = GetAuctionItemClasses()
+
+	L.Trinket = getglobal('INVTYPE_TRINKET')
+
+	L.Devices, L.Explosives = select(10, GetAuctionItemSubClasses(6))
+
+	L.SimpleGem = select(8, GetAuctionItemSubClasses(7))
+end
+
+L.Normal = 'Обычное'
+L.Equipment = 'Обмундирование'
+L.Keys = 'Ключи'
+L.Trade = 'Товары'
+L.Ammo = 'Боеприпасы'
+L.Shards = 'Камни'
+L.SoulShard = 'Камни душ'
+L.Usable = 'Расходуемые'
---
+++ Combuctor_Config/sets.lua
@@ -0,0 +1,134 @@
+﻿--[[
+	Sets.lua
+		Basic set types for combuctor
+
+		setRule(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+--]]
+
+-- Stolen from OneBag, since my bitflag knowledge could be better
+-- BAGTYPE_QUIVER = Quiver + Ammo
+local BAGTYPE_QUIVER = 0x0001 + 0x0002 
+-- BAGTYPE_SOUL = Soul Bags
+local BAGTYPE_SOUL = 0x004
+-- BAGTYPE_PROFESSION = Leather + Inscription + Herb + Enchanting + Engineering + Gem + Mining
+local BAGTYPE_PROFESSION = 0x0008 + 0x0010 + 0x0020 + 0x0040 + 0x0080 + 0x0200 + 0x0400 
+
+local CombuctorSet = Combuctor:GetModule('Sets')
+local L = LibStub('AceLocale-3.0'):GetLocale('Combuctor')
+
+
+
+--the all category (player, bagType filters)
+CombuctorSet:Register(L.All, 'Interface/Icons/battleground_strongbox_gold_horde', function() return true end)
+CombuctorSet:RegisterSubSet(L.All, L.All)
+CombuctorSet:RegisterSubSet(L.Normal, L.All, nil, function(player, bagType) 
+	return bagType and bagType == 0 
+end)
+CombuctorSet:RegisterSubSet(L.Keys, L.All, nil, function(player, bagType) 
+	return bagType and bagType == 256 
+end)
+
+
+--equipment filters (armor, weapon, trinket)
+local function isEquipment(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return (type == L.Armor or type == L.Weapon)
+end
+CombuctorSet:Register(L.Equipment, 'Interface/Icons/inv_shoulder_plate_raidwarrior_r_01', isEquipment)
+CombuctorSet:RegisterSubSet(L.All, L.Equipment)
+
+local function isArmor(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Armor and equipLoc ~= 'INVTYPE_TRINKET'
+end
+CombuctorSet:RegisterSubSet(L.Armor, L.Equipment, nil, isArmor)
+
+local function isWeapon(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Weapon
+end
+CombuctorSet:RegisterSubSet(L.Weapon, L.Equipment, nil, isWeapon)
+
+local function isTrinket(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return equipLoc == 'INVTYPE_TRINKET'
+end
+CombuctorSet:RegisterSubSet(L.Trinket, L.Equipment, nil, isTrinket)
+
+
+--usable items
+local function isUsable(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	if type == L.Consumable then
+		return true
+	elseif type == L.TradeGood then
+		if subType == L.Devices or subType == L.Explosives then
+			return true
+		end
+	end
+end
+CombuctorSet:Register(L.Usable, 'Interface/Icons/trade_alchemy_potiond4', isUsable)
+CombuctorSet:RegisterSubSet(L.All, L.Usable)
+
+local function isConsumable(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Consumable
+end
+CombuctorSet:RegisterSubSet(L.Consumable, L.Usable, nil, isConsumable)
+
+local function isDevice(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.TradeGood
+end
+CombuctorSet:RegisterSubSet(L.Devices, L.Usable, nil, isDevice)
+
+
+--quest items
+local function isQuestItem(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Quest
+end
+CombuctorSet:Register(L.Quest, 'Interface/Icons/inv_misc_book_01', isQuestItem)
+CombuctorSet:RegisterSubSet(L.All, L.Quest)
+
+
+--trade goods + gems
+local function isTradeGood(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	if type == L.TradeGood then
+		return not(subType == L.Devices or subType == L.Explosives)
+	end
+	return type == L.Recipe or type == L.Gem
+end
+CombuctorSet:Register(L.TradeGood, 'Interface/Icons/inv_fabric_frostweave_imbuedbolt', isTradeGood)
+CombuctorSet:RegisterSubSet(L.All, L.TradeGood)
+
+local function isTradeGood(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.TradeGood
+end
+CombuctorSet:RegisterSubSet(L.TradeGood, L.TradeGood, nil, isTradeGood)
+
+local function isGem(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Gem
+end
+CombuctorSet:RegisterSubSet(L.Gem, L.TradeGood, nil, isGem)
+
+local function isRecipe(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Recipe
+end
+CombuctorSet:RegisterSubSet(L.Recipe, L.TradeGood, nil, isRecipe)
+
+
+--ammo (all bags)
+local function isProjectile(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Projectile
+end
+CombuctorSet:Register(L.Projectile, 'Interface/Icons/inv_ammo_arrow_04', isProjectile)
+CombuctorSet:RegisterSubSet(L.All, L.Projectile)
+
+
+--shards (all bags)
+local function isShard(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return link and (link:match('%d+') == '6265')
+end
+CombuctorSet:Register(L.SoulShard, 'Interface/Icons/INV_Misc_Gem_Amethyst_02', isShard)
+CombuctorSet:RegisterSubSet(L.All, L.SoulShard)
+
+
+--misc
+local function isMiscItem(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+	return type == L.Misc and (link:match('%d+') ~= '6265')
+end
+CombuctorSet:Register(L.Misc, 'Interface/Icons/misc_rune_pvp_random', isMiscItem)
+CombuctorSet:RegisterSubSet(L.All, L.Misc)
